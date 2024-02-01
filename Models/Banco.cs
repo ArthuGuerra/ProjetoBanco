301651409450630da3c9128494be716709c87f55
ArthuGuerra <arthurflavioguerra@gmail.com>
2024-02-01 19:53:59 -0300

    subindo o projeto Banco

---
+++ Models/Banco.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ProjetoBancoYou.Models
+{
+    public class Banco
+    {
+        private string nome;
+
+        public string NumeroConta { get; private set;}
+        public decimal Saldo { get; private set; }
+
+        public Banco(string numeroConta, string nome)
+        {   
+            NumeroConta = numeroConta;
+            Nome = nome;
+            Saldo = 0;
+        }
+        
+        public Banco (string numeroConta, string nome, decimal saldo) :this(numeroConta,nome)
+        {
+            Saldo = saldo; 
+        }
+
+        public string Nome 
+        {
+            get => nome;  
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Nome inválido");
+                }
+                else 
+                {
+                    nome = value;
+                }
+            }
+        }
+
+
+        public void Depositar()
+        {
+            Console.WriteLine("Ola, seja bem-vindo!");
+            Console.WriteLine("Quanto o senhor(a) deseja depositar ? ");
+            decimal valor = Convert.ToDecimal(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Saldo += valor;
+            Console.WriteLine("Depósito feito com sucesso!");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Saldo atual: R${Saldo.ToString("F2",CultureInfo.InvariantCulture)}");
+
+        }
+
+        public void Sacar()
+        {
+            Console.WriteLine("Ola, seja bem-vindo!");
+            Console.WriteLine("Quanto o senhor(a) deseja sacar ? ");
+            decimal valor = Convert.ToDecimal(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Saldo -= valor + 5;  // taxa de 5 reais por saque
+            Console.WriteLine("Saque feito com sucesso!");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Saldo atual: R${Saldo.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+
+        public void Dados()
+        {
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine($"Nome do Titular: {nome}");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Numero da Conta: {NumeroConta}");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Saldo Atual: R${Saldo.ToString("F2",CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using ProjetoBancoYou.Models;
+
+Console.WriteLine("Olá, seja bem vindo ao banco Guerra. Iremos fazer seu cadastro");
+Console.WriteLine("Digite seu nome por favor!");
+string nome = Console.ReadLine();
+Console.WriteLine("\n");
+Console.WriteLine("Digite 8 números da conta nesse formato por favor ####-#### ");
+string numeroConta = Console.ReadLine();
+
+Console.WriteLine("O senhor(a) deseja fazer um depósito inicial ? ");
+string resposta = Console.ReadLine();
+
+
+if (resposta == "s" || resposta == "S")
+{
+    Console.WriteLine("Digite o valor do depósito por favor");
+    decimal saldo = Convert.ToDecimal(Console.ReadLine(),CultureInfo.InvariantCulture);
+    Banco cliente = new Banco(numeroConta,nome,saldo);
+
+    Console.WriteLine("Depósito feito com sucesso!");
+    Console.WriteLine("\n");
+    Console.WriteLine($"Saldo atual: {saldo.ToString("F2",CultureInfo.InvariantCulture)}");
+    Console.WriteLine("\n");
+
+    cliente.Dados();
+    Console.WriteLine("\n");
+    cliente.Depositar();
+    Console.WriteLine("\n");
+    cliente.Sacar();
+    Console.WriteLine("\n");
+    cliente.Dados();
+    
+
+}
+else
+{
+    Banco cliente = new Banco(numeroConta, nome);
+
+    cliente.Dados();
+    Console.WriteLine("\n");
+    cliente.Depositar();
+    Console.WriteLine("\n");
+    cliente.Sacar();
+    Console.WriteLine("\n");
+    cliente.Dados();
+}
+
+
+
+
+
+// é isso, espero que tenham gostado. deixa o like tmj
